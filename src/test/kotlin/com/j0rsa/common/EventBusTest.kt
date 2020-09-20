00f2b8542b85fca6085050d2bb5fafe27828530c
keyridan <keyridan@gmail.com>
2020-09-20 23:08:41 +0200

    small test fixes

--- src/test/kotlin/com/j0rsa/common/EventBusTest.kt
+++ src/test/kotlin/com/j0rsa/common/EventBusTest.kt
@@ -1,49 +1,47 @@
 package com.j0rsa.common
 
-import arrow.fx.extensions.io.concurrent.sleep
 import io.kotest.assertions.timing.eventually
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
-import java.time.Duration
 import java.util.*
 import kotlin.time.ExperimentalTime
 import kotlin.time.milliseconds
 import kotlin.time.seconds
+import com.j0rsa.common.KafkaEventBus.send
+import com.j0rsa.common.KafkaEventBus.listen
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
-object KafkaEventBus: EventBus {
-    override val brokers: String = "localhost:9092"
-    override val consumerGroup: String = "test"
+object KafkaEventBus : EventBus {
+	override val brokers: String = "localhost:9092"
+	override val consumerGroup: String = "test"
 }
 
-object StringKafkaSyntax: KafkaSyntax<String> {
-    override val serialize: (String) -> String = { it }
-    override val deserialize: (String) -> String = { it }
+object StringKafkaSyntax : KafkaSyntax<String> {
+	override val serialize: (String) -> String = { it }
+	override val deserialize: (String) -> String = { it }
 }
 
 @ExperimentalTime
-class EventBusTest: StringSpec({
+class EventBusTest : StringSpec({
 
-    "send a message" {
-        with(KafkaEventBus) {
-            StringKafkaSyntax.send("topic1", "test")
-        }
-    }
+	"send a message" {
+		StringKafkaSyntax.send("topic1", "test")
+	}
 
-    "receive a message" {
-        with(KafkaEventBus) {
-            val randomMessage = UUID.randomUUID().toString()
-            val topic = "topic3"
-            var received = false
-            Thread {
-                StringKafkaSyntax.listen(topic, true) {
-                    if (it == randomMessage) received = true
-                }
-            }.start()
-            StringKafkaSyntax.send(topic, randomMessage)
+	"receive a message" {
+		val randomMessage = UUID.randomUUID().toString()
+		val topic = "topic3"
+		var received = false
+		GlobalScope.launch {
+			StringKafkaSyntax.listen(topic, true) {
+				if (it == randomMessage) received = true
+			}
+		}
+		StringKafkaSyntax.send(topic, randomMessage)
 
-            eventually(2.seconds, 100.milliseconds) {
-                received shouldBe true
-            }
-        }
-    }
+		eventually(2.seconds, 100.milliseconds) {
+			received shouldBe true
+		}
+	}
 })
